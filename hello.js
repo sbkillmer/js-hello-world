b479e9465e95eb52b4259c30d9e694bac0f38704
sbkillmer <sethk1600@gmail.com>
2023-12-21 17:13:03 -0600

    Modified Hello World JS Test

--- hello.js
+++ hello.js
@@ -4,19 +4,29 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log("Nice to meet you.")
+console.log(10+10)
+console.log("The Quick Brown Fox"); console.log("pause for dramatic effect")
+console.log("Jumped Over " + "The Lazy Dog")
+console.log('Quoth the Raven "Nevermore"')
+console.log(" ")
+console.log(10*10)
+let x = "Goodbye!"
+console.log(x)
 
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
-   1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
-   4. Remove the semi-colon, ;.
-   5. Print a number. (Bonus: Print two numbers added together).
-   6. Print multiple messages one after the other.
+   1. Change the message that is printed. (Done)
+   2. Figure out what the parentheses do. Will the code work without them? (No)
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? (Seems like both are needed) Is there a difference between using a single or a double quote (' vs. ")? (Both work)
+   4. Remove the semi-colon, ;. (Still works)
+   5. Print a number. (Bonus: Print two numbers added together). (Works after removing quotation marks)
+   6. Print multiple messages one after the other.(Done using the semicolon)
    7. Print two messages on the same line.
+   (See Jumped Over + The Lazy Dog)
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
-   9. Other. You choose!
+   (Double quotes within a single quote line)
+   9. Other. You choose! (Created a line break with empty quotes, tested multiplication, attempt define and call variable)
 */
